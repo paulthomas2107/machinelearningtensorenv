17ddc931c532725a13fb2d2ea8d40eb2de3d5f36
Paul Thomas <paulthomas2107@gmail.com>
2022-08-28 16:17:53 +0100

    More.....

--- KNN/cars.py
+++ KNN/cars.py
@@ -8,3 +8,26 @@ from sklearn import linear_model, preprocessing
 data = pd.read_csv("car.data")
 print(data.head())
 
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+main = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+carclass = le.fit_transform(list(data["class"]))
+
+print(buying)
+predict = "class"
+
+x = list(zip(buying, main, door, persons, lug_boot, safety))
+y = list(carclass)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+print(x_train, y_test)
+
+
+
+
+
