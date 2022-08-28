3173fbc623c562c93b7517019eda3772286b61ee
Paul Thomas <paulthomas2107@gmail.com>
2022-08-28 15:48:40 +0100

    More..

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import keras
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -20,10 +23,21 @@ y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+best = 0
+for _ in range(30):
+
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print("Accuracy : ", acc)
+
+    if acc > best:
+        best = acc
+    with open("studentmodel.pickle", "wb") as f:
+        pickle.dump(linear, f)
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 print("Coefficients :", linear.coef_)
 print("Intercepts   :", linear.intercept_)
@@ -32,3 +46,10 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = 'G1'  # Abscences, failures, studytime, G1 etc
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
